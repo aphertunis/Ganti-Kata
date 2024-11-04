3ba78d88fdb01c493f125e48bfb0ca0a7f768c7b
Nur Riskon Abdan Syakuro <23176031@mhs.stmik-tegal.ac.id>
2024-11-04 19:09:33 +0700

    Add files via upload

---
+++ latihan28.java
@@ -0,0 +1,29 @@
+//Nama      : Nur Riskon Abdan Syakuro
+//  NIM       : 23176031
+//  Prodi     : Sistem Informasi
+//  Deskripsi :   program ini untuk mengganti kata yang diinputkan user
+
+
+import java.util.Scanner;
+
+public class latihan28 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+          System.out.print("Masukkan Kalimat: ");
+          String kalimat = scanner.nextLine();
+
+          System.out.print("Ganti Kata: ");
+          String kataAsli = scanner.nextLine();
+
+          System.out.print("Menjadi Kata: ");
+          String kataBaru = scanner.nextLine();
+
+          String kalimatBaru = kalimat.replaceAll(kataAsli, kataBaru);
+
+          System.out.println("\n====Hasil Formatting====");
+          System.out.println("Kalimat awal: " + kalimat);
+          System.out.println("Kalimat baru: " + kalimatBaru);
+        }
+    }
+}
